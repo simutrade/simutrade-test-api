add1aacfc9fd9dc0189287704da5f01f103b8048
patrick <1069515501@qq.com>
2023-02-12 16:53:00 +0800

    commit

--- controllers/game-controller.js
+++ controllers/game-controller.js
@@ -1,3 +1,5 @@
+const db = require("../db");
+
 module.exports = {
     hello: async function (req, res) {
         let response = {
@@ -12,5 +14,27 @@ module.exports = {
     /*
      * TODO: You can add your controller actors here.
      */
-    
+    displayGameInfo: async function (req, res) {
+        let gameId = req.params.id;
+        let gameInfo = await db.game.findGameInfoById(gameId);
+        console.log(gameInfo);
+        if (gameInfo) {
+            //if the game is found
+            return res.json({
+                id: gameId,
+                gamename: gameInfo["name"],
+                time: Date.now(),
+                gamedescription:gameInfo["description"],
+                createTime: gameInfo["create_time_utc"],
+                startTime: gameInfo["start_time_utc"],
+                endTime: gameInfo["end_time_utc"],
+                init_cash: gameInfo["init_cash"],
+                userName: gameInfo["display_name"],
+                userEmail: gameInfo["email"]
+            });
+            //return res.json(gameInfo)
+        } else {
+            return res.json("Game not found");
+        }
+    }
 }
